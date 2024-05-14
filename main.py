b4081920f061627ab2ae66d1112df07ab4663b46
chebupelka8 <stpzamyatin@gmail.com>
2024-05-14 12:19:32 +0500

    Started working on the app (Version of QtHarmony is 0.2.4)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from qtharmony.widgets import PushButton, DropDownMenu, DigitalEntry, Entry, RadioButton, CheckBox
 from qtharmony.windows import MainWindow
 from qtharmony.constructor import Initialization
+from qtharmony.src.core.theme import ThemeManager
 
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout
 
@@ -8,28 +9,15 @@ import sys
 
 
 Initialization.init(sys.argv)
+ThemeManager.change_theme("Dark-Default")
 
 
 class Window(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
-        self.mainLayout = QVBoxLayout()
-        self.firstLayout = QHBoxLayout()
-        self.secondLayout = QHBoxLayout()
-        self.thirdLayout = QHBoxLayout()
-
-        for i in (self.firstLayout, self.secondLayout, self.thirdLayout):
-            self.mainLayout.addLayout(i)
-
-        self.firstLayout.addWidget(PushButton("Button"))
-        self.firstLayout.addWidget(DropDownMenu([str(i) * 10 for i in range(10)]))
-
-        self.secondLayout.addWidget(DigitalEntry())
-        self.secondLayout.addWidget(Entry())
-
-        self.thirdLayout.addWidget(RadioButton("LOOOOOOOOOOOL!"))
-        self.thirdLayout.addWidget(CheckBox("HAHAHAH?"))
+        self.mainLayout = QHBoxLayout()
+        self.mainLayout.addWidget(PushButton("Button"))
 
         self.setLayout(self.mainLayout)
 
